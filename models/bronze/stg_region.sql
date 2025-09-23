54c6ac0e9899ff397667a71cbdd4732c593d88ab
alexmjxfb@gmail.com <alexmjxfb@gmail.com>
2025-09-23 13:58:27 +0000

    Fix stg_region query where no select was defined

--- models/bronze/stg_region.sql
+++ models/bronze/stg_region.sql
@@ -2,6 +2,7 @@ with src as (
   select *, 
     current_timestamp() as _ingest_ts,
     '{{ invocation_id }}' as _ingest_batch_id,
-    lower(hash( to_varchar(R_REGIONKEY))) as _record_hash
+    lower( hash( to_varchar(R_REGIONKEY) ) ) as _record_hash
   from {{ source('staging','region') }}
 )
+select * from src
