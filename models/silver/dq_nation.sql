5116346b026045127ed9c37d210f1bcb80f25a33
alexmjxfb@gmail.com <alexmjxfb@gmail.com>
2025-09-24 07:45:58 +0000

    Fix unfinished SQL statement

--- models/silver/dq_nation.sql
+++ models/silver/dq_nation.sql
@@ -9,10 +9,7 @@ with
             n_nationkey as id,
             n_name as "name",
             n_comment as n_comment,
-            c_nationkey as nation_key,
-            c_phone as phone,
-            c_acctbal as account_balance,
-            c_mktsegment as market_segment
+            n_regionkey as region_id
 
         from source
 
