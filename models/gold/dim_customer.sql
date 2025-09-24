e39b1f03179487b5b6c652f2e03d79f9aa21db46
alexmjxfb@gmail.com <alexmjxfb@gmail.com>
2025-09-24 12:37:19 +0000

    Refactor customer dimension with nation and region

--- models/gold/dim_customer.sql
+++ models/gold/dim_customer.sql
@@ -5,12 +5,17 @@
 
 select
 
-    id,
-    "address",
-    "name",
-    nation_id
-    phone,
-    account_balance,
-    market_segment
+    c.id as customer_id,
+    c."name" as customer_name,
+    c."address",
+    n."name" as nation_name,
+    r."name" as region_name,
+    c.phone,
+    c.account_balance,
+    c.market_segment
 
-from {{ ref('dq_customer') }}
+from {{ ref('dq_customer') }} as c
+join {{ ref('dq_nation') }} as n
+on c.nation_id = n.id
+join {{ ref('dq_region') }} as r
+on n.region_id = r.id
